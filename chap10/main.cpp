5a131a3d59ae0f6a88576f1c682b6e0229887779
unknown <>
2016-11-24 16:50:21 +0900

    ExArray class implement, practice Template

--- chap10/main.cpp
+++ chap10/main.cpp
@@ -1,72 +1,74 @@
-#include <process.h>
-#include <iostream>
-#include "KeyEvent.h"
-using namespace std;
-class Game {
-private:
+#pragma once
+#include "Frame.h"
+#include "ExArray.h"
+void main() {
+	/*
+	using namespace std;
+	class Game {
+	private:
 	int x;
 	int y;
 	int direction;
 	void gotoXY(int x, int y) {
-		COORD pos;
-		pos.X = x;
-		pos.Y = y;
-		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 	}
-public:
+	public:
 	Game() {
-		x = 5;
-		y = 5;
-		direction = 0;
+	x = 5;
+	y = 5;
+	direction = 0;
 	}
 	void setDirection(int i) {
-		direction = i;
+	direction = i;
 	}
 	void move() {
-		if (direction == 37) {
-			x--;
-		}
-		else if (direction == 38) {
-			y--;
-		}
-		else if (direction == 39) {
-			x++;
-		}
-		else if (direction == 40) {
-			y++;
-		}
-		system("cls");
-		this->gotoXY(x, y);
-		cout << x << "  " << y;
-		cout << "бс";
+	if (direction == 37) {
+	x--;
 	}
-};
-Game* game = new Game();
-unsigned int __stdcall mythread(void*)
-{
+	else if (direction == 38) {
+	y--;
+	}
+	else if (direction == 39) {
+	x++;
+	}
+	else if (direction == 40) {
+	y++;
+	}
+	system("cls");
+	this->gotoXY(x, y);
+	cout << x << "  " << y;
+	cout << "бс";
+	}
+	};
+	Game* game = new Game();
+	unsigned int __stdcall mythread(void*)
+	{
 
 	int i = 0;
 	while (1) {
-		game->move();
-		Sleep(500);
+	game->move();
+	Sleep(500);
 	}
 	return 0;
-}
-unsigned int __stdcall keyEvent(void*) {
+	}
+	unsigned int __stdcall keyEvent(void*) {
 	KeyEvent k;
 	int i;
 	while (1) {
-		i = k.getKey();
-		switch (i) {
-		case 37: game->setDirection(i); break;
-		case 38: game->setDirection(i); break;
-		case 39: game->setDirection(i); break;
-		case 40: game->setDirection(i); break;
-		}
+	i = k.getKey();
+	switch (i) {
+	case 37: game->setDirection(i); break;
+	case 38: game->setDirection(i); break;
+	case 39: game->setDirection(i); break;
+	case 40: game->setDirection(i); break;
+	}
 	}
 	return 0;
-}
-int main() {
+	}
+	int main() {
 	HANDLE handleA, handleB;
 	handleA = (HANDLE)_beginthreadex(0, 0, &mythread, (void*)0, 0, 0);
 	handleB = (HANDLE)_beginthreadex(0, 0, &keyEvent, (void*)0, 0, 0);
@@ -78,4 +80,29 @@ int main() {
 	CloseHandle(handleB);
 
 	return 0;
+	*/
+	/*
+	try {
+	Frame *f1 = new Frame;
+	Frame *f2 = new Frame[10];
+
+	delete f1;
+	delete f2;
+	}
+	catch (string msg) {
+	cout << "Error msg :" << msg << endl;
+	}
+	*/
+
+	ExArray<int> arr1(3);
+	ExArray<char> arr2(4);
+	ExArray<double> arr3(5);
+
+	arr1.addData(2);
+	arr2.addData('A');
+	arr3.addData(32.12);
+
+	arr1.printData();
+	arr2.printData();
+	arr3.printData();
 }
